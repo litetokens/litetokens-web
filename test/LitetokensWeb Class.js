304446077644c0cfcfa99e0ca37aea4e8591293b
marlonhanks <owner@kedia.co>
2022-02-27 10:31:45 +0000

    updated addressing

--- test/LitetokensWeb Class.js
+++ test/LitetokensWeb Class.js
@@ -8,7 +8,7 @@ const FULL_NODE_API = 'https://api.shasta.litescan.org';
 const SOLIDITY_NODE_API = 'https://api.shasta.litescan.org';
 const EVENT_API = 'https://api.shasta.litescan.org';
 const PRIVATE_KEY = 'da146374a75310b9666e834ee4ad0866d6f4035967bfc76217c5a495fff9f0d0';
-const ADDRESS_HEX = '30928c9af0651632157ef27a2cf17ca72c575a4d28';
+const ADDRESS_HEX = '30928c9af0651632157ef27a2cf17ca72c575a4d21';
 const ADDRESS_BASE58 = 'LYaqMeF8c8eUHqjktYVQSAoJ933YthjNA1';
 
 const createInstance = () => {
@@ -210,7 +210,7 @@ describe('LitetokensWeb Instance', function () {
 
             assert.equal(litetokensWeb.defaultPrivateKey, false);
             assert.equal(litetokensWeb.defaultAddress.hex, '30928c9af0651632157ef27a2cf17ca72c575a4d21');
-            assert.equal(litetokensWeb.defaultAddress.base58, 'LYaqMeF8c8eUHqjktYVQSAoJ933YthjNA1');
+            assert.equal(litetokensWeb.defaultAddress.base58, 'LetuHVRUTGwtaKXZUzaSkwrcm7sYCtbxuD');
         });
 
         it('should not reset the private key if the address matches', function () {
